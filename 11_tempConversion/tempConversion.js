2d44198ed9b2fbc0e237d038dab7286541aaf1f2
William Liu <will.liu1705@gmail.com>
2025-10-24 08:11:28 -0400

    Complete 11-tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsiusTemp = (temp - 32) * 5 / 9;
+
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheitTemp = temp * 9 / 5 + 32;
+  return Number(fahrenheitTemp.toFixed(1));
 };
 
 // Do not edit below this line
