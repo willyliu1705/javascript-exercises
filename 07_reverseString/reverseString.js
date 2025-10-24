fcb6130053114a17431f8ad88f053370e46c57a6
William Liu <will.liu1705@gmail.com>
2025-10-24 06:27:53 -0400

    Complete 07-reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringReversed = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    stringReversed += string[i];
+  }
+  return stringReversed;
 
+  // Alternate solution
+  // return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
