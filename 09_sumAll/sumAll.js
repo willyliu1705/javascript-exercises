7ee851f90a7b5dd865be15dedb1d5c00b77e3ea1
William Liu <will.liu1705@gmail.com>
2025-10-24 07:32:23 -0400

    Complete 09-sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
 
+  let stringA = String(a);
+  let stringB = String(b);
+
+  if (a < 0 || b < 0 ||
+    typeof a !== "number" || typeof b !== "number" ||
+    stringA.includes(".") || stringB.includes(".")) {
+    return "ERROR";
+  }
+
+  let startValue = a < b ? a : b;
+  let endValue = a > b ? a : b;
+
+  let sum = 0;
+  for (let i = startValue; i <= endValue; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
