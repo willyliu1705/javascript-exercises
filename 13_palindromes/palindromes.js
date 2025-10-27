09531d43ed67323f197f974ab801cd8d4585919e
William Liu <will.liu1705@gmail.com>
2025-10-26 21:26:19 -0400

    Complete 13-palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+  let cleanedString = string.toLowerCase().split("").filter((char) => alphanumerical.includes(char)).join("");
+
+  let reversedString = cleanedString.split("").reverse().join("");
+
+  return cleanedString === reversedString;
 };
 
 // Do not edit below this line
