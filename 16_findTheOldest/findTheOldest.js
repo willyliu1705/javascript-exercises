b212b05da1408aed144fa3f0f8d0b32bf84f94c0
William Liu <will.liu1705@gmail.com>
2025-10-26 22:01:50 -0400

    Complete 16-findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arrayOfPeopleObjects) {
+  return arrayOfPeopleObjects.reduce(
+    (oldest, person) => {
+      const thisYear = new Date().getFullYear();
+      if (oldest.yearOfDeath === undefined) {
+        oldest.yearOfDeath = thisYear;
+      }
+      if (person.yearOfDeath === undefined) {
+        person.yearOfDeath = thisYear;
+      }
+      return oldest.yearOfDeath - oldest.yearOfBirth > person.yearOfDeath - person.yearOfBirth ? oldest : person;
+    }
+  );
 };
 
 // Do not edit below this line
